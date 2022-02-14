500b7d540ba4798a215e9d7289a62473a4d57484
Josh Mei <jmei@snares-02.cs.wisc.edu>
2022-02-14 00:59:46 -0600

    fixed bugs in cla_16b, shifter & ALU test passed

--- hw2/hw2_2/alu.v
+++ hw2/hw2_2/alu.v
@@ -26,5 +26,42 @@ module alu (InA, InB, Cin, Oper, invA, invB, sign, Out, Zero, Ofl);
     output                      Zero; // Signal if Out is 0
 
     /* YOUR CODE HERE */
+
+    //Opcode //Function     //Operation/
+    //000    //rll          //Rotate left logical
+    //001    //sll          //Shift left logical
+    //010    //sra          //Shift right arithmetic
+    //011    //srl          //Shift right logical
+    //100    //ADD          //A + B
+    //101    //AND          //A AND B
+    //110    //OR           //A OR B
+    //111    //XOR          //A XOR B
+
+
+    wire [15:0] InAA; //
+    wire [15:0] InBB;
+    assign InAA = invA ? ~InA : InA;
+    assign InBB = invB ? ~InB : InB;
     
+    wire [15:0] shifter_out;
+    wire [15:0] cla_16b_out;
+    wire c_out;
+    shifter shifter(.In(InAA), .ShAmt(InBB[3:0]), .Oper(Oper[1:0]), .Out(shifter_out));
+    cla_16b cla_16b(.sum(cla_16b_out), .c_out(c_out), .a(InAA), .b(InBB), .c_in(Cin));
+
+    wire Ofl_signed;
+    wire Ofl_unsigned;
+    assign Ofl_signed = ( (sign == 1'b1) & ( (InAA[15] == InBB[15]) & (InAA[15] != cla_16b_out[15]) ) );
+    assign Ofl_unsigned = ( (c_out == 1'b1) & (sign == 1'b0) );
+    assign Ofl = Ofl_signed ^ Ofl_unsigned;
+    //assign Ofl =  ((sign == 1'b1) & ((InAA[15] == InBB[15]) & (InAA[15] != cla_16b_out[15]))) | ((c_out == 1'b1) & (sign == 1'b0));
+    assign Zero = ~(|Out);
+
+    assign Out = 
+        (Oper[2] == 1'b0  ) ? shifter_out :
+        (Oper    == 3'b100) ? cla_16b_out :
+        (Oper    == 3'b101) ? InAA & InBB :
+        (Oper    == 3'b110) ? InAA | InBB :
+        (Oper    == 3'b111) ? InAA ^ InBB : InAA;
+
 endmodule
