218a9faa7bf860bd8c7a7b822a8f596c4516cb77
Josh Mei <jmei@royal-22.cs.wisc.edu>
2022-02-20 14:27:14 -0600

    finish debugging in hw3

--- hw3/hw3_2/regFile_bypass.v
+++ hw3/hw3_2/regFile_bypass.v
@@ -23,5 +23,12 @@ module regFile_bypass (
    output        err;
 
    /* YOUR CODE HERE */
+   wire [15:0] read1Data_rf, read2Data_rf;
+   regFile rf(.clk(clk), .rst(rst), .read1RegSel(read1RegSel), .read2RegSel(read2RegSel),
+              .writeRegSel(writeRegSel), .writeData(writeData), .writeEn(writeEn),
+              .read1Data(read1Data_rf), .read2Data(read2Data_rf), .err(err));
+
+   assign read1Data = (writeEn & (read1RegSel == writeRegSel)) ? writeData : read1Data_rf;
+   assign read2Data = (writeEn & (read2RegSel == writeRegSel)) ? writeData : read2Data_rf;
 
 endmodule
