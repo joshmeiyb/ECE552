9a611eba0791a8d61160e4cb6a711bac35d955f6
Josh Mei <jmei@vm-instunix-09.cs.wisc.edu>
2022-03-21 17:12:04 -0500

    added gitignore

--- hw4/hw4_1/p1-1.asm
+++ hw4/hw4_1/p1-1.asm
@@ -1,1 +1,9 @@
 // Write your assembly program for Problem 1 (a) #1 here.
+
+//Test seq instruction
+//11100 sss ttt ddd xx | SEQ Rd, Rs, Rt | if (Rs == Rt) then Rd <- 1 else Rd <- 0
+
+lbi r1, 0
+lbi r2, 0
+seq r3, r1, r2  //r3 should be 1
+halt
--- hw4/hw4_1/p1-3.asm
+++ hw4/hw4_1/p1-3.asm
@@ -1,1 +1,5 @@
 // Write your assembly program for Problem 1 (a) #3 here.
+
+//Test seq instruction
+//11100 sss ttt ddd xx | SEQ Rd, Rs, Rt | if (Rs == Rt) then Rd <- 1 else Rd <- 0
+
